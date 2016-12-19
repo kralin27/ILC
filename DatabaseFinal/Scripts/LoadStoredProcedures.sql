5a3bc5941c038acc5f5cb285ba0e11cfabea4d63
kralin27 <kralin27@evergreen.edu>
2016-12-19 15:28:09 -0800

    Delete LoadStoredProcedures.sql

--- DatabaseFinal/Scripts/LoadStoredProcedures.sql
+++
@@ -1,4 +0,0 @@
-C:/Eddie/LindseyProject/DatabaseFinal/StoredProcedures/prcWeatherReadingMissing.sql
-C:/Eddie/LindseyProject/DatabaseFinal/StoredProcedures/prcWeatherReading.sql
-C:/Eddie/LindseyProject/DatabaseFinal/StoredProcedures/prcWeatherReadingsDate.sql
-C:/Eddie/LindseyProject/DatabaseFinal/StoredProcedures/prcWeatherReadingsMeasure.sql
